66ca667440e930f295e371dee2baf8cba96fe72a
Giorgio <giorgio.dodesini.it@gmail.com>
2019-12-21 16:24:58 +0100

    login and signup form at very center of the screen; all the dashboard is fixed

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -2,38 +2,44 @@
   <v-container fluid v-if="user">
     <v-row align="start" justify="center">
       <v-col lg="4" md="6">
-        <v-card>
+        <v-card min-width="200">
           <v-card-title>Last Person Detected</v-card-title>
-          <v-card-subtitle>Name: {{lastPersonDetected.name}} - Degree: {{lastPersonDetected.degree}} - Detected At: {{user.events[0].date | date}}</v-card-subtitle>
-          <v-img :src="user.events[0].imageUrl" max-height="200px"></v-img>
-        </v-card>
-      </v-col>
-      <v-col lg="4" md="6" v-if="mostDetectedUser">
-        <v-card>
-          <v-card-title>Most Detected Person</v-card-title>
-          <v-card-subtitle>Name: {{mostDetectedUser.name}} - Degree: {{mostDetectedUser.degree}} - Counter: {{mostDetectedUser.counter}}</v-card-subtitle>
-          <v-img :src="mostDetectedUser.imageUrl" height="200"></v-img>
+          <template v-if="user.events.length != 0">
+            <v-card-subtitle>Name: {{lastPersonDetected.name}} - Degree: {{lastPersonDetected.degree}} - Detected At: {{user.events[0].date | date}}</v-card-subtitle>
+            <v-img :src="user.events[0].imageUrl" max-height="200px"></v-img>
+          </template>
         </v-card>
       </v-col>
       <v-col lg="4" md="6">
-        <v-card>
-          <v-card-title>Last Events</v-card-title>
-          <v-container fluid>
-            <v-expansion-panels accordion multiple focusable popout class="py-4">
-              <v-expansion-panel v-for="(event) in user.events.slice(0,3)" :key="event._id">
-                <v-expansion-panel-header>{{event.description}}</v-expansion-panel-header>
-                <v-expansion-panel-content>
-                  <v-container fluid>
-                    <v-row justify="end">
-                      <v-btn class="mx-2" small outlined to="/history">
-                        <v-icon small>mdi-open-in-new</v-icon>
-                      </v-btn>
-                    </v-row>
-                  </v-container>
-                </v-expansion-panel-content>
-              </v-expansion-panel>
-            </v-expansion-panels>
-          </v-container>
+        <v-card min-width="200">
+          <v-card-title>Most Detected Person</v-card-title>
+          <template v-if="mostDetectedUser > 0">
+            <v-card-subtitle>Name: {{mostDetectedUser.name}} - Degree: {{mostDetectedUser.degree}} - Counter: {{mostDetectedUser.counter}}</v-card-subtitle>
+            <v-img :src="mostDetectedUser.imageUrl" height="200"></v-img>
+          </template>
+        </v-card>
+      </v-col>
+      <v-col lg="4" md="6">
+        <v-card min-width="200">
+            <v-card-title>Last Events (max 3)</v-card-title>
+            <template v-if="user.events.length != 0">
+              <v-container fluid>
+                <v-expansion-panels accordion multiple focusable popout>
+                  <v-expansion-panel v-for="(event) in user.events.slice(0,3)" :key="event._id">
+                    <v-expansion-panel-header>{{event.description}}</v-expansion-panel-header>
+                    <v-expansion-panel-content>
+                      <v-container fluid>
+                        <v-row justify="end">
+                          <v-btn class="mx-2" small outlined to="/history">
+                            <v-icon small>mdi-open-in-new</v-icon>
+                          </v-btn>
+                        </v-row>
+                      </v-container>
+                    </v-expansion-panel-content>
+                  </v-expansion-panel>
+                </v-expansion-panels>
+              </v-container>
+          </template>
         </v-card>
       </v-col>
     </v-row>
@@ -48,7 +54,6 @@ export default {
       user.events.sort(function(a, b) {
         return new Date(b.date) - new Date(a.date);
       });
-      console.log(user);
       return user;
     },
     mostDetectedUser() {
