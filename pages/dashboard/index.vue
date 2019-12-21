dfb2b3f0e4b6a8e566e0c1eadfbe7dd8da186d4d
Giorgio <giorgio.dodesini.it@gmail.com>
2019-12-21 16:58:04 +0100

    open events from dashboard

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -13,7 +13,7 @@
       <v-col lg="4" md="6">
         <v-card min-width="200">
           <v-card-title>Most Detected Person</v-card-title>
-          <template v-if="mostDetectedUser > 0">
+          <template v-if="mostDetectedUser">
             <v-card-subtitle>Name: {{mostDetectedUser.name}} - Degree: {{mostDetectedUser.degree}} - Counter: {{mostDetectedUser.counter}}</v-card-subtitle>
             <v-img :src="mostDetectedUser.imageUrl" height="200"></v-img>
           </template>
@@ -30,7 +30,7 @@
                     <v-expansion-panel-content>
                       <v-container fluid>
                         <v-row justify="end">
-                          <v-btn class="mx-2" small outlined to="/history">
+                          <v-btn small outlined @click="goToEventDetails(event)">
                             <v-icon small>mdi-open-in-new</v-icon>
                           </v-btn>
                         </v-row>
@@ -43,11 +43,53 @@
         </v-card>
       </v-col>
     </v-row>
-    <v-row align="start" justify="center"></v-row>
+    <v-overlay :value="eventOverlay">
+        <v-container fluid>
+          <v-row align="center" justify="center">
+            <v-col lg="12" md="12" sm="12">
+              <v-card class="elevation-12" min-width="200" max-width="300">
+                <v-toolbar color="teal lighten-1" dark flat>
+                  <v-toolbar-title>Event Details</v-toolbar-title>
+                  <v-spacer></v-spacer>
+                  <v-btn icon @click="closeEventDetails">
+                    <v-icon>mdi-close</v-icon>
+                  </v-btn>
+                </v-toolbar>
+                <v-card-subtitle>Details:</v-card-subtitle>
+                <v-card-text>
+                  <ul>
+                    <li>
+                      <b>Detected At</b>
+                      : {{openedEvent.date | date}}
+                    </li>
+                    <li>
+                      <b>Description</b>
+                      : {{openedEvent.description}}
+                    </li>
+                  </ul>
+                  <v-img :src="openedEvent.imageUrl" max-height="300px"></v-img>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                    <v-btn icon :href="openedEvent.imageUrl" class="mb-0">
+                      <v-icon>fas fa-download</v-icon>
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-overlay>
   </v-container>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      openedEvent: {},
+      eventOverlay: false
+    }
+  },
   computed: {
     user() {
       let user = this.$store.getters.getUser;
@@ -80,6 +122,14 @@ export default {
         }
       }
       return maxPerson;
+    },
+    goToEventDetails(event) {
+      this.openedEvent = event;
+      this.eventOverlay = true;
+    },
+    closeEventDetails() {
+      this.openedEvent = {};
+      this.eventOverlay = false;
     }
   }
 };
